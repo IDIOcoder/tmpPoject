99aa4efddd6c348dd5d03080033e8c97f7c46c13
TirTir <73577283+TirTir@users.noreply.github.com>
2024-03-29 11:36:59 +0900

    domain.Diary

---
+++ Diary.java
@@ -0,0 +1,24 @@
+package NewProject.Diary.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class Diary {
+    @Id @GeneratedValue
+    @Column(name = "diary", updatable = false)
+    private int WriteNum;
+    @Column(name = "write_date", nullable = false)
+    private String WriteDate;
+    @Column(name = "title", nullable = false)
+    private String title;
+    private String text;
+
+}
