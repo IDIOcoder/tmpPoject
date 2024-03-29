05e029e2a574b59658ef417b8f72af52e07691d4
IDIOcoder <108709378+IDIOcoder@users.noreply.github.com>
2024-03-29 11:36:00 +0900

    dto, repository

---
+++ DiaryDTO.java
@@ -0,0 +1,24 @@
+package NewProject.Diary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class DiaryDTO {
+    private String writeDate;   // 작성일
+    private int writeNum;       // 작성순서
+    private String title;       // 기록 이름
+    private String text;        // 기록 내용
+
+    public Diary toEntity(){
+        return Diary.builder()
+                .writeDate
+                .writeNum
+                .title
+                .text
+                .build();
+    }
+}
