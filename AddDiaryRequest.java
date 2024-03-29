c8fe0b7489b7a96a85dcb5cba23f99158474aa88
IDIOcoder <108709378+IDIOcoder@users.noreply.github.com>
2024-03-29 13:18:38 +0900

    Updated AddDiaryRequest / JSW

--- AddDiaryRequest.java
+++ AddDiaryRequest.java
@@ -10,16 +10,16 @@ import lombok.NoArgsConstructor;
 @Getter
 public class AddDiaryRequest {
     private String writeDate;   // 작성일
-    private int writeNum;       // 작성순서
+    private long writeNum;       // 작성순서
     private String title;       // 기록 이름
     private String text;        // 기록 내용
 
     public Diary toEntity(){
         return Diary.builder()
-                .writeDate
-                .writeNum
-                .title
-                .text
+                .writeDate(writeDate)
+                .writeNum(writeNum)
+                .title(title)
+                .text(text)
                 .build();
     }
 }
