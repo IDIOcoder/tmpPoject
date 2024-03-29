d05a3c1a3aa8e14abfe9a14def971a6b5bb1cb65
IDIOcoder <108709378+IDIOcoder@users.noreply.github.com>
2024-03-29 21:52:31 +0900

    Delete repository directory

--- repository/DiaryRepository.java
+++
@@ -1,7 +0,0 @@
-package NewProject.Diary.repository;
-
-import NewProject.Diary.domain.Diary;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiaryRepository extends JpaRepository<Diary, Long> {
-}
